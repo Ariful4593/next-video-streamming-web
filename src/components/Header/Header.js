f0a1e5257ca89ed59ef4743b1867922d95b336b9
Ariful Islam <arifkhagoria2015@gmail.com>
2023-04-12 00:18:04 +0600

    Main slider width issue fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,36 +7,69 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faBell, faUser } from '@fortawesome/free-solid-svg-icons'
 import { BurgerMenu } from '@/ui/BurgerMenu/BurgerMenu'
 export const Header = () => {
+    const [active, setActive] = React.useState('');
     return (
-        <div className="container  ">
-                <div className='row mt-4 mb-4'>
-                    <div className={`col-3 ${Styles.logo_part}`}>
-                        <Image
-                            className={Styles.logo}
-                            src={logo}
-                            alt="Next.js Logo"
-                            width={180}
-                            height={37}
-                            priority
-                        />
-                    </div>
-                    {/* <BurgerMenu/> */}
-                    <div className={`col-6 ${Styles.menu_part}`}>
-                        <ul>
-                            <li><Link href='/'>Home</Link></li>
-                            <li><Link href='/movies'>Movies</Link></li>
-                            <li><Link href='/tvshows'>TV Shows</Link></li>
-                            <li><Link href='/videos'>Videos</Link></li>
-                            <li><Link href='/blog'>Blog</Link></li>
-                            <li><Link href='/pages'>Pages</Link></li>
-                        </ul>
-                    </div>
-                    <div className={`col-3 ${Styles.profile_part}`}>
-                        <FontAwesomeIcon className='icon' icon={faSearch} />
-                        <FontAwesomeIcon className='icon' icon={faBell} />
-                        <FontAwesomeIcon className='icon' icon={faUser} />
-                    </div>
+        <div className={`${Styles.container} container`}>
+            <div className={`row mt-4 mb-4 ${Styles.large_device}`}>
+                <div className={`col-3 ${Styles.logo_part}`}>
+                    <Image
+                        className={Styles.logo}
+                        src={logo}
+                        alt="Next.js Logo"
+                        width={180}
+                        height={37}
+                        priority
+                    />
+                </div>
+                <div className={`col-6 ${Styles.menu_part}`}>
+                    <ul>
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/movies'>Movies</Link></li>
+                        <li><Link href='/tvshows'>TV Shows</Link></li>
+                        <li><Link href='/videos'>Videos</Link></li>
+                        <li><Link href='/blog'>Blog</Link></li>
+                        <li><Link href='/pages'>Pages</Link></li>
+                    </ul>
+                </div>
+                <div className={`col-3 ${Styles.profile_part}`}>
+                    <FontAwesomeIcon className='icon' icon={faSearch} />
+                    <FontAwesomeIcon className='icon' icon={faBell} />
+                    <FontAwesomeIcon className='icon' icon={faUser} />
                 </div>
             </div>
+            <div className={`row mt-4 mb-4 ${Styles.small_device}`}>
+                <div className={`col-3 ${Styles.burger_menu_part}`}>
+                    <BurgerMenu setActive={setActive} />
+                </div>
+
+                {/* Submenu for when clicked burger menu */}
+                <div className={`col-12 ${Styles.submenu} ${active ? 'd-block' : 'd-none'}`}>
+                    <ul>
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/movies'>Movies</Link></li>
+                        <li><Link href='/tvshows'>TV Shows</Link></li>
+                        <li><Link href='/videos'>Videos</Link></li>
+                        <li><Link href='/blog'>Blog</Link></li>
+                        <li><Link href='/pages'>Pages</Link></li>
+                    </ul>
+                </div>
+
+                <div className={`col-5 ${Styles.logo_part}`}>
+                    <Image
+                        className={Styles.logo}
+                        src={logo}
+                        alt="Next.js Logo"
+                        width={180}
+                        height={37}
+                        priority
+                    />
+                </div>
+                <div className={`col-4 ${Styles.profile_part}`}>
+                    <FontAwesomeIcon className='icon' icon={faSearch} />
+                    <FontAwesomeIcon className='icon' icon={faBell} />
+                    <FontAwesomeIcon className='icon' icon={faUser} />
+                </div>
+            </div>
+        </div>
     )
 }
--- src/ui/BurgerMenu/BurgerMenu.js
+++ src/ui/BurgerMenu/BurgerMenu.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Styles from './BurgerMenu.module.css';
 
-export const BurgerMenu = () => {
+export const BurgerMenu = ({ setActive }) => {
+
     return (
         <div>
             <input type="checkbox" id={Styles.checkbox} />
-            <label htmlFor={Styles.checkbox} className={Styles.toggle}>
+            <label htmlFor={Styles.checkbox} className={`${Styles.toggle}`} onClick={() => setActive(prev => !prev)} >
                 <div className={Styles.bars} id={Styles.bar1}></div>
                 <div className={Styles.bars} id={Styles.bar2}></div>
                 <div className={Styles.bars} id={Styles.bar3}></div>
